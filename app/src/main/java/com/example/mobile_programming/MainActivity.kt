174ed84b0e35bcdfc0d607171307812857ca8f45
seok28 <seokjoonsoo@gmail.com>
2022-11-16 21:49:39 +0900

    Connecting to a Firebase & Login Ui

--- app/src/main/java/com/example/mobile_programming/MainActivity.kt
+++ app/src/main/java/com/example/mobile_programming/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import com.example.mobile_programming.Auth.LoginActivity
+import com.example.mobile_programming.Auth.MyActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom.*
 
@@ -11,10 +14,13 @@ import kotlinx.android.synthetic.main.bottom.*
 class MainActivity : AppCompatActivity() {
 
     internal lateinit var viewpager : ViewPager
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
 
         val img = arrayOf(
             R.drawable.ai,
@@ -60,6 +66,13 @@ class MainActivity : AppCompatActivity() {
 
         my_page.setOnClickListener {
 
+            if(auth.currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }else {
+                val intent = Intent(this, MyActivity::class.java)
+                startActivity(intent)
+            }
 
         }
     }
